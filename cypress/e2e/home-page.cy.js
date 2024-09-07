d538f8f09da0f8a520579e265d2a07304a325894
John Kapantzakis <kapantzak@gmail.com>
2024-09-07 23:29:01 +0300

    Add pages response tests

--- cypress/e2e/home-page.cy.js
+++ cypress/e2e/home-page.cy.js
@@ -4,8 +4,46 @@ describe("Home page", () => {
   const baseUrl = Cypress.env("baseUrl");
 
   context("Request checks", () => {
-    it("gets 200 response", () => {
-      cy.request(Cypress.env("baseUrl")).its("status").should("eq", 200);
+    [
+      {
+        url: `${Cypress.env("baseUrl")}`,
+        page: "",
+        status: 200,
+      },
+      {
+        url: `${Cypress.env("baseUrl")}/shop`,
+        page: "shop",
+        status: 200,
+      },
+      {
+        url: `${Cypress.env("baseUrl")}/blog`,
+        page: "blog",
+        status: 200,
+      },
+      {
+        url: `${Cypress.env("baseUrl")}/about`,
+        page: "about",
+        status: 200,
+      },
+      {
+        url: `${Cypress.env("baseUrl")}/contacts`,
+        page: "contact",
+        status: 200,
+      },
+      {
+        url: `${Cypress.env("baseUrl")}/foo`,
+        page: "foo",
+        status: 404,
+      },
+    ].forEach(({ url, page, status }) => {
+      it(`/${page} returns ${status}`, () => {
+        cy.request({
+          url,
+          failOnStatusCode: false,
+        })
+          .its("status")
+          .should("eq", status);
+      });
     });
   });
 
